8c4e0d65aa14e38acce73a2a345c4c58a84ca626
Megan Weijiang <meganweijiang@utexas.edu>
2018-08-24 14:48:33 -0500

    remove unecessary files

--- client/src/data.js
+++
@@ -1,6 +0,0 @@
-const data = [
-  { _id: 1, name: 'Bryan', description: 'Wow this is neat', updatedAt: new Date(), createdAt: new Date() },
-  { _id: 2, name: 'You', description: 'You\'re __right!__', updatedAt: new Date(), createdAt: new Date() },
-];
-
-export default data;
